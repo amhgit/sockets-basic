13a541a76e498f0d6cc02d896635367ddd8539d5
amhidalgo <ahidalgo@verizon.net>
2016-02-29 01:23:47 -0600

    Added listener to front-end

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
+
 io.on('connection', function() {
 	console.log('user connected via socket.io');
 });
+
 http.listen(PORT, function() {
     console.log('Server started');
 });
